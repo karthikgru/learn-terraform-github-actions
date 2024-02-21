d45bd10aedc5f9000bb0697754acb2d17738b2b5
karthikgru <karthikkumaraguru@outlook.com>
2024-02-22 00:00:50 +0530

    update

--- main.tf
+++ main.tf
@@ -37,10 +37,6 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-locals {
-  cluster_name = "guru-eks-${random_string.suffix.result}"
-}
-
 resource "random_string" "suffix" {
   length  = 8
   special = false
