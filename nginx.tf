e8511e8823f6e1ac80e80c287ad86b566d1c4e64
karthikgru <karthikkumaraguru@outlook.com>
2024-02-22 00:23:27 +0530

    updated cl_name

--- nginx.tf
+++ nginx.tf
@@ -1,20 +1,9 @@
-data "terraform_remote_state" "eks" {
-  backend = "remote"
-
-  config = {
-    organization = "demo-kk"
-    workspaces = {
-      name = "gh-actions-demo"
-    }
-  }
-}
-
 data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
+  host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
