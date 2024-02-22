b45be2e89fe36ed69f47afb2674d6e50784a16c7
karthikgru <karthikkumaraguru@outlook.com>
2024-02-22 12:22:21 +0530

    argocd

---
+++ argocd-setup/helm.tf
@@ -0,0 +1,26 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+  values           = [file("values/argocd.yaml")]
+}
+# helm install argocd -n argocd -f values/argocd.yaml
